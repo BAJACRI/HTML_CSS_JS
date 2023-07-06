82408f62cfeb11d1b5abf310e20b8d4104f29820
bajacri <bajacri@yahoo.es>
2023-07-06 15:33:48 -0600

    Debugging details in finished project for Bootstrap v3.4.1

--- restaurante_chino_Bootstrap_v3.4.1/js/ajax-utils.js
+++ restaurante_chino_Bootstrap_v3.4.1/js/ajax-utils.js
@@ -1,73 +1,67 @@
 (function (global) {
-	
-	// Set up a namespace for our utility
-	var ajaxUtils = {};
 
-	// Returns HTTP request object
-	function getRequestObject() {
-
-		if (global.XMLHttpRequest)
-			{
-				return ( new XMLHttpRequest() );
-			}
-		else if (global.ActiveXObject)
-			{
-				// For every old IE browsers (optional)
-				return ( new ActiveXObject("Microsoft.XMLHTTP") );
-			}
-			else
-				{
-					global.alert("Ajax is not supported!");
-					return(null);
-				}
-
-	} // END OF function getRequestObject()
+// Set up a namespace for our utility
+var ajaxUtils = {};
 
 
-	//Make an AJAX GET request to 'requestUrl'
-	ajaxUtils.sendGetRequest = function ( requestUrl, responseHandler, isJsonResponse )
-								{
-									var request = getRequestObject();
+// Returns an HTTP request object
+function getRequestObject() {
+  if (global.XMLHttpRequest) {
+    return (new XMLHttpRequest());
+  } 
+  else if (global.ActiveXObject) {
+    // For very old IE browsers (optional)
+    return (new ActiveXObject("Microsoft.XMLHTTP"));
+  } 
+  else {
+    global.alert("Ajax is not supported!");
+    return(null); 
+  }
+}
 
-									request.onreadystatechange = function()
-																	{
-																		handleResponse( request, responseHandler, isJsonResponse );
-																	};
 
-									request.open( "GET", requestUrl, true );
-									// For debugging purposes
-									// console.log(requestUrl);
-									request.send( null ); // for POST only
-								};
+// Makes an Ajax GET request to 'requestUrl'
+ajaxUtils.sendGetRequest = 
+  function(requestUrl, responseHandler, isJsonResponse) {
+    var request = getRequestObject();
+    request.onreadystatechange = 
+      function() { 
+        handleResponse(request, 
+                       responseHandler,
+                       isJsonResponse); 
+      };
+    request.open("GET", requestUrl, true);
+    request.send(null); // for POST only
+  };
 
 
-	// Only calls user provided 'responseHandler' function
-	// if response is ready and not an error
-	function handleResponse( request, responseHandler, isJsonResponse ) {
-		
-		if ( (request.readyState == 4) && (request.status == 200) )
-			{
-				// Default to isJsonResponse = true
-				if (isJsonResponse == undefined)
-					{
-						isJsonResponse = true;
-					}
+// Only calls user provided 'responseHandler'
+// function if response is ready
+// and not an error
+function handleResponse(request,
+                        responseHandler,
+                        isJsonResponse) {
+  if ((request.readyState == 4) &&
+     (request.status == 200)) {
 
-				if (isJsonResponse)
-					{
-						responseHandler( JSON.parse(request.responseText) );
-					}
-				else
-					{
-						responseHandler( request.responseText );
-					}
-			}
+    // Default to isJsonResponse = true
+    if (isJsonResponse == undefined) {
+      isJsonResponse = true;
+    }
 
-	} // END OF function handleResponse( request, responseHandler )
+    if (isJsonResponse) {
+      responseHandler(JSON.parse(request.responseText));
+    }
+    else {
+      responseHandler(request.responseText);
+    }
+  }
+}
 
-	
-	// Expose utility to the global object
-	global.$ajaxUtils = ajaxUtils;
 
-} // END OF function (global)
-) (window);
+// Expose utility to the global object
+global.$ajaxUtils = ajaxUtils;
+
+
+})(window);
+
